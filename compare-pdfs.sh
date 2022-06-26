239505aa28b7b0612aa275dcc4ff571724e70efb
Simon Schädler <simon.schaedler@simworld.ch>
2022-06-26 13:26:23 +0200

    Setup project

---
+++ compare-pdfs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+###############################################################################
+# Descrition: 	Lightweight bashscript to compare PDF files
+# Copyright: 	Simon Schädler © 2022
+# Licence:		GNU GPLv3 
+# Usage: 		./compare-pdfs.sh [candidate_file] [demand_file] [compare_file]
+###############################################################################
+
