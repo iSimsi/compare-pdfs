a6a3bf2e4b797f8a428c7621969da1734093a78c
Simon Schädler <simon.schaedler@simworld.ch>
2022-06-28 01:18:15 +0200

    Release Version

--- compare-pdfs.sh
+++ compare-pdfs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ###############################################################################
-# Descrition: 	Lightweight bashscript to compare PDF files
-# Copyright: 	Simon Schädler © 2022
-# Licence:		GNU GPLv3 
-# Usage: 		./compare-pdfs.sh [candidate_file_path] [demand_file_path]
+# Descrition: Lightweight bashscript to compare PDF files
+# Copyright:  Simon Schädler © 2022
+# Licence:    GNU GPLv3 
+# Usage:      ./compare-pdfs.sh [candidate_file_path] [demand_file_path]
 ###############################################################################
 
 # VARIABLES ###################################################################
@@ -32,9 +32,6 @@ workPath="$scriptPath/work"
 # output path
 outputPath="$scriptPath/output"
 
-# prefix for output files
-outputFilePrefix="compare"
-
 # prefix for candidate files
 candidateFilePrefix="candidate"
 
@@ -44,8 +41,8 @@ demandFilePrefix="demand"
 # log path
 logPath="$scriptPath/log"
 
-# collect that names of .png files to be converted to a single .pdf file
-compareFiles=""
+# return code
+rc=0
 
 
 # Functions ###################################################################
@@ -53,31 +50,31 @@ compareFiles=""
 # function do_log()
 # generate log entrys
 function do_log() {
-	printf "%-19s %-5s %-s\n" "$(date '+%Y-%m-%d %H:%M:%S')" "${severity}" "${message}"
+    printf "%-19s %-5s %-s\n" "$(date '+%Y-%m-%d %H:%M:%S')" "${severity}" "${message}"
 }
 
 # check_file_exists()
 # check if pdf file exists
 # Parameter 1: filePath string
 function check_pdf_exists() {
-	if [ ! -f $1 ]; then
-		severity="ERROR"
-	   	message="File $1 not found, will terminate the script"
-	   	do_log | tee -a ${logFile}
-	   	exit 1
-	fi
+    if [ ! -f $1 ]; then
+        severity="ERROR"
+        message="File $1 not found, will terminate the script"
+        do_log | tee -a ${logFile}
+        exit 1
+    fi
 }
 
 # check_file_readable()
 # check if pdf file is readable
 # Parameter 1: filePath string
 function check_pdf_readable() {
-	if [ ! -r $1 ]; then
-		severity="ERROR"
-	   	message="File $1 is not readable, will terminate the script"
-	   	do_log | tee -a ${logFile}
-	   	exit 1
-	fi
+    if [ ! -r $1 ]; then
+        severity="ERROR"
+        message="File $1 is not readable, will terminate the script"
+        do_log | tee -a ${logFile}
+        exit 1
+    fi
 }
 
 # function split_pdf()
@@ -86,37 +83,60 @@ function check_pdf_readable() {
 # Parameter 2: fileName string
 # Parameter 3: filePrefix string
 function split_pdf() {
-	pdftk $1 burst output ${actaulWorkPath}/${3}_${2%.*}_page_%04d.pdf 2>/dev/null
-	rcpt=$?
-	if [[ ${rcpt} -ne 0 ]]; then
-		severity="ERROR"
-	   	message="Failed to split ${3} pdf file: ${1}, will terminate the script"
-	   	do_log | tee -a ${logFile}
-	   	exit 1
-	else 
-		severity="INFO"
-	   	message="Split succesfull ${3} pdf file: ${1}"
-	   	do_log | tee -a ${logFile}
-	fi
+    pdftk $1 burst output ${actaulWorkPath}/${3}_${2%.*}_page_%04d.pdf 2>/dev/null
+    rcpt=$?
+    if [[ ${rcpt} -ne 0 ]]; then
+        severity="ERROR"
+        message="Failed to split ${3} pdf file: ${1}, will terminate the script"
+        do_log | tee -a ${logFile}
+        exit 1
+    fi
 }
 
 # function convert_png()
 # converting the pdf files to png
 # Parameter 1: filePath string
-# Parameter 2: fileName string
+# Parameter 2: filepath string
 function convert_png() {
-	pdftoppm -png -singlefile -r  $resolution "${1}" "${actaulWorkPath}/${2%.*}" 
-	rcpo=$?
-	if [[ ${rcpo} -ne 0 ]]; then
-		severity="ERROR"
-	   	message="Failed to convert ${3} pdf file to png: ${1}, will terminate the script"
-	   	do_log | tee -a ${logFile}
-	   	exit 1
-	else 
-		severity="INFO"
-	   	message="Converted succesfull ${3} pdf file tp png: ${1}"
-	   	do_log | tee -a ${logFile}
-	fi
+    pdftoppm -png -singlefile -r  $resolution "${1}" "${actaulWorkPath}/${2%.*}" 
+    rcpo=$?
+    if [[ ${rcpo} -ne 0 ]]; then
+        severity="ERROR"
+        message="Failed to convert pdf file to png: ${1}, will terminate the script"
+        do_log | tee -a ${logFile}
+        exit 1
+    fi
+}
+
+# function compare_png()
+# comparing the png files
+# Parameter 1: candidate filePath string
+# Parameter 2: demand filePath string
+# Parameter 3: comapre filePath string
+# Parameter 4: output filenName string
+function compare_png() {
+    compare "${1}" "${2}" "${3}"
+    rcco1=$?
+    if [[ ${rcco1} -ne 1 ]]; then
+        severity="ERROR"
+        message="Comparing ${1} and ${2} failed, will terminate the script"
+        do_log | tee -a ${logFile}
+        echo $rcco1
+        exit 1
+    else
+        convert "${3}" "${outputPath}/${4%.*}.pdf"
+        rcco2=$?
+        if [[ ${rcco2} -ne 0 ]]; then
+            severity="ERROR"
+            message="Creating compare output file ${outputPath}/${4%.*}.pdf failed, will terminate the script"
+            do_log | tee -a ${logFile}
+            exit 1
+        else 
+            severity="INFO"
+            message="Created successfull compare output file ${outputPath}/${4%.*}.pdf"
+            do_log | tee -a ${logFile}
+        fi
+    fi
 }
 
 
@@ -124,63 +144,72 @@ function convert_png() {
 
 # Check if log dir exists or create it and set the Logfile name
 if [[ ! -d ${logPath} ]]; then
-	mkdir -p ${logPath}
-	rclp=$?
-	if [[ ${rcld} -ne 0 ]]; then
-	   	severity="ERROR"
-	   	message="Failed to create the log directory: ${logPath}, will terminate the script"
-	   	do_log
-	   	exit 1
-	else 
-		logFile="${logPath}/compare-pdfs_${actualDate}-${actualTime}.log"
-	fi
+    mkdir -p ${logPath}
+    rclp=$?
+    if [[ ${rcld} -ne 0 ]]; then
+        severity="ERROR"
+        message="Failed to create the log directory: ${logPath}, will terminate the script"
+        do_log
+        exit 1
+    else 
+        logFile="${logPath}/compare-pdfs_${actualDate}-${actualTime}.log"
+    fi
 else
-	logFile="${logPath}/compare-pdfs_${actualDate}-${actualTime}.log"
+    logFile="${logPath}/compare-pdfs_${actualDate}-${actualTime}.log"
 fi
 
 # Check if work dir exists or create it
 if [[ ! -d ${workPath} ]]; then
-	mkdir -p ${workPath}
-	rcwp=$?
-	if [[ ${rcwp} -ne 0 ]]; then
-	   	severity="ERROR"
-	   	message="Failed to create the work directory: ${workPath}, will terminate the script"
-	   	do_log | tee -a ${logFile}
-	   	exit 1
-	fi
+    mkdir -p ${workPath}
+    rcwp=$?
+    if [[ ${rcwp} -ne 0 ]]; then
+        severity="ERROR"
+        message="Failed to create the work directory: ${workPath}, will terminate the script"
+        do_log | tee -a ${logFile}
+        exit 1
+    fi
 fi
 
 # Check if output dir exists or create it
 if [[ ! -d ${outputPath} ]]; then
-	mkdir -p ${outputPath}
-	rcop=$?
-	if [[ ${rcop} -ne 0 ]]; then
-	   	severity="ERROR"
-	   	message="Failed to create the work directory: ${outputPath}, will terminate the script"
-	   	do_log | tee -a ${logFile}
-	   	exit 1
-	fi
+    mkdir -p ${outputPath}
+    rcop=$?
+    if [[ ${rcop} -ne 0 ]]; then
+        severity="ERROR"
+        message="Failed to create the work directory: ${outputPath}, will terminate the script"
+        do_log | tee -a ${logFile}
+        exit 1
+    fi
 fi
 
 # Check parameter count
 if [[ $# -ne 2 ]]; then
-	severity="ERROR"
-	message="Wrong count of script parameters!"
-	do_log | tee -a ${logFile}
-	severity="INFO"
-	message="Usage: ./compare-pdfs.sh [candidate_file_path] [demand_file_path]"
-	do_log | tee -a ${logFile}
-	exit 1
+    severity="ERROR"
+    message="Wrong count of script parameters!"
+    do_log | tee -a ${logFile}
+    severity="INFO"
+    message="Usage: ./compare-pdfs.sh [candidate_file_path] [demand_file_path]"
+    do_log | tee -a ${logFile}
+    exit 1
 fi
 
 
 # MAIN ########################################################################
+severity="INFO"
+message="Start processing" 
+do_log | tee -a ${logFile}
 
 # Check if file in candidateFilePath exists
 check_pdf_exists ${candidateFilePath}
+severity="INFO"
+message="Candidate file is: ${candidateFilePath}" 
+do_log | tee -a ${logFile}
 
 # Check if file in demandFilePath exists
 check_pdf_exists ${demandFilePath}
+severity="INFO"
+message="Demand file is: ${demandFilePath}" 
+do_log | tee -a ${logFile}
 
 # Check if file in candidateFilePath is readable
 check_pdf_readable ${candidateFilePath}
@@ -201,7 +230,7 @@ actaulWorkPath="${workPath}/${actualDate}-${actualTime}"
 mkdir -p ${actaulWorkPath}
 
 # bursting the candidate pdf into individual pages
-split_pdf ${candidateFilePath} ${candidateFile} ${candidateFilePrefix}	
+split_pdf ${candidateFilePath} ${candidateFile} ${candidateFilePrefix}  
 
 # bursting the demand pdf into individual pages
 split_pdf ${demandFilePath} ${demandFile} ${demandFilePrefix}
@@ -220,55 +249,78 @@ demandPageCount=${#demandPages[@]}
 
 # compare page counts
 if [[ $candidatePageCount -ne  $demandPageCount ]]; then
-	severity="ERROR"
-	message="Page length of candidate ($candidatePageCount) and demand ($demandPageCount) file is different" 
-	do_log | tee -a ${logFile}
-	exit 1
+    severity="ERROR"
+    message="Page length of candidate ($candidatePageCount) and demand ($demandPageCount) file is different, will terminate the script" 
+    do_log | tee -a ${logFile}
+    exit 2
+else
+    severity="INFO"
+    message="Page length of candidate ($candidatePageCount) and demand ($demandPageCount) file is eqaul" 
+    do_log | tee -a ${logFile}
 fi
 
 # loop over the individual pages of the candidate file
 for ((i = 0; i < ${#candidatePages[@]}; i++)); do
     
     # current page files to convert
- 	candidatePage="${candidatePages[$i]}"
-	demandPage="${demandPages[$i]}"
-	candidatePageName=$(basename ${candidatePage})
-	demandPageName=$(basename ${demandPage})
+    candidatePage="${candidatePages[$i]}"
+    demandPage="${demandPages[$i]}"
+    candidatePageName=$(basename ${candidatePage})
+    demandPageName=$(basename ${demandPage})
+    pageNumber=$(( $i + 1 ))
 
-	# convert current candidate page file to png
-	convert_png ${candidatePage} ${candidatePageName}
+    # convert current candidate page file to png
+    convert_png ${candidatePage} ${candidatePageName}
 
-	# convert current  demand page file to png
-	convert_png ${demandPage} ${demandPageName}
+    # convert current  demand page file to png
+    convert_png ${demandPage} ${demandPageName}
 
-	# create the checksums for the two PNG files
-	candidateCecksum=$(sha256sum ${candidatePage%.*}.png | awk '{print $1}')
-	severity="INFO"
-	message="Checksum candidate file: ${candidateCecksum}" 
-	do_log | tee -a ${logFile}
+    # create the checksums for the two PNG files
+    candidatePageCecksum=$(sha256sum ${candidatePage%.*}.png | awk '{print $1}')
+    severity="INFO"
+    message="Checksum candidate page ${pageNumber}: ${candidatePageCecksum}" 
+    do_log | tee -a ${logFile}
 
-	demandCecksum=$(sha256sum ${demandPage%.*}.png | awk '{print $1}')
-	severity="INFO"
-	message="Checksum demand file: ${demandCecksum}" 
-	do_log | tee -a ${logFile}
+    demandPageCecksum=$(sha256sum ${demandPage%.*}.png | awk '{print $1}')
+    severity="INFO"
+    message="Checksum demand page ${pageNumber}: ${demandPageCecksum}" 
+    do_log | tee -a ${logFile}
 
-	# compare the checksums
-	if [[ ${demandCecksum} = ${demandCecksum} ]]; then
-		severity="INFO"
-		message="Candidate and demand page have same checksum, Test passed!" 
-		do_log | tee -a ${logFile}
-	else
-		severity="WARN"
-		message="Candidate and demand page have  diffrent checksum, create compare file" 
-		do_log | tee -a ${logFile}
+    # compare the checksums
+    if [[ ${candidatePageCecksum} = ${demandPageCecksum} ]]; then
+        severity="INFO"
+        message="Candidate page ${pageNumber} and demand page ${pageNumber} have same checksum" 
+        do_log | tee -a ${logFile}
+    else
+        severity="WARN"
+        message="Candidate page ${pageNumber} and demand page ${pageNumber} have diffrent checksum, create compare file" 
+        do_log | tee -a ${logFile}
+        rc=3
 
-		# create compare file
-		compare "${candidatePage%.*}.png" "${demandPage%.*}.png" "$outputFilePrefix_${candidatePage%.*}.png"
-	fi
+        # create compare file name for error page
+        comparePageName=${candidatePageName/candidate/compare}
+
+        # create compare file name for error page
+        compare_png "${candidatePage%.*}.png" "${demandPage%.*}.png" "$actaulWorkPath/${comparePageName%.*}.png" "${comparePageName%.*}.png"
+    fi
 done
+
+if [[ ${rc} -ne 0 ]]; then
+    severity="WARN"
+    message="At least 1 page of the candidate file has differences to the demand file" 
+    do_log | tee -a ${logFile}
+else
+    severity="INFO"
+    message="No differences between candidate file anddemand file detected, test passed!" 
+    do_log | tee -a ${logFile}
+fi
 
 # clean up
 rm -rf $actaulWorkPath
 
-exit 1
+severity="INFO"
+message="End processing" 
+do_log | tee -a ${logFile}
+
+exit ${rc} 
 
